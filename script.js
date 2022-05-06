dab0f7ee88471390a08812cf10c22f4885473b06
Gabriel Teles <101151753+GabrielFTeles@users.noreply.github.com>
2022-05-06 19:29:26 -0300

    Day 4/5

---
+++ script.js
@@ -0,0 +1,56 @@
+const header = document.querySelector('#fixed-header');
+const menu = document.querySelector('.menu');
+
+addEventListener('scroll', onScroll);
+onScroll();
+
+function onScroll() {
+    showHeaderOnScroll();
+    showBackToTopOnScroll();
+    changeBackToTopButtonColor();
+}
+
+function showHeaderOnScroll() {
+    scrollY > 0 ? header.classList.add('scroll') : header.classList.remove('scroll');
+}
+
+function showBackToTopOnScroll() {
+    scrollY > 500 ? backToTopButton.classList.add('show') : backToTopButton.classList.remove('show');
+}
+
+function changeBackToTopButtonColor() {
+    scrollY > 4089 ?  backToTopButton.classList.add('invert-color') : backToTopButton.classList.remove('invert-color');
+}
+
+function openMenu() {
+    document.body.classList.add('menu-expanded');
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded');
+}
+
+
+
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+}).reveal(`
+    #home,
+    .home-content,
+    .home-content #woman-image,
+    .home-content .stats,
+    #services,
+    .services-content h4,
+    .services-content h2,
+    .services-content .card,
+    #about,
+    .about-content h4,
+    .about-content h2,
+    .about-content p,
+    .about-content img,
+    #contact,
+    .contact-content h2,
+    .contact-content p,
+    .contact-content img`);
